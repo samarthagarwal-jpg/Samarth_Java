3a05f1e617620c30534505011b97db3cef6340ba
Samarth <shivaagarwal34@gmail.com>
2026-09-12 22:03:28 +0530

    init commit

--- ExceptionClass.java
+++ ExceptionClass.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+class MyException extends Exception
+{
+    public String toString()
+    {
+        return super.toString() + "I am toString";
+    }
+    public String getString()
+    {
+        return super.getString() + "I am getMessage";
+    }
+}
 public class ExceptionClass {
     public static void main(String[] args) {
-        
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a= sc.nextInt();
+        if(a<99)
+        {
+            try {
+                throw new MyException();
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
     }
     
 }
